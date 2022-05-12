5c98e4b2248284a2a795e8eb74562047761f7559
Abi <awhittle@oregonstate.edu>
2022-05-12 12:57:56 -0700

    Theres now content on all pages

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ app.set("view engine", 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const homeStartingContent = "";
-const aboutContet = "";
-const contactContent = "";
+const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 app.get("/", function(req, res) {
-    res.render("home");
+    res.render("home", {homeStartingContent: homeStartingContent});
 });
 
 app.get("/about", function(req, res) {
-    res.render("about");
+    res.render("about", {aboutContent: aboutContent});
 });
 
 app.get("/contact", function(req, res) {
-    res.render("contact");
+    res.render("contact", {contactContent: contactContent});
 });
 
 app.listen(3000, function(req, res) {
